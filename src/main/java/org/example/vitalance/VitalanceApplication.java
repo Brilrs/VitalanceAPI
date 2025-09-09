8f28d7a4879c7db7fbfdbb7dc3f93e502ccb45f8
Brilrs <bricm304@gmail.com>
2025-09-09 10:32:05 -0500

    Se agrega el proyecto base

---
+++ src/main/java/org/example/vitalance/VitalanceApplication.java
@@ -0,0 +1,13 @@
+package org.example.vitalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitalanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/vitalance/VitalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vitalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
