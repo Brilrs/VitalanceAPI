a281a2d2dbca2deb8b31b3ae53c514364d021065
BoboTheProgrammer <robbiedulantot@gmail.com>
2025-09-14 21:13:32 -0500

    NUevatabla

--- src/main/java/org/example/vitalance/entidades/User.java
+++ src/main/java/org/example/vitalance/entidades/User.java
@@ -20,6 +20,7 @@ import java.util.List;
 @NoArgsConstructor
 @Table(name="Users")
 public class User {
+    //dwhde
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUser;
