27a9cfa2a4ea314a4dc45abb1a24136ae520c253
Brillite <bricm304@gmail.com>
2025-10-05 20:19:04 -0500

    Arreglo de importacion

--- src/main/java/org/example/vitalance/controllers/NotificacionPacienteController.java
+++ src/main/java/org/example/vitalance/controllers/NotificacionPacienteController.java
@@ -4,6 +4,7 @@ import org.example.vitalance.dtos.NotificacionPacienteDTO;
 import org.example.vitalance.servicios.NotificacionPacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
